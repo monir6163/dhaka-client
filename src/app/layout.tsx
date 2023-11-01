99a5ada209cc299a59384a33f58a1947fecb5f0d
netstermonir <monirhossain6163@gmail.com>
2023-11-02 00:39:12 +0600

    modern nav

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,29 +4,29 @@ import NavBar from "./components/Shared/NavBar";
 import "./globals.css";
 
 export const metadata = {
-    title: "Dhaka Client",
-    description: "A Next.js starter with Tailwind CSS and TypeScript",
+  title: "Next.js 14",
+  description: "A Next.js starter with Tailwind CSS and TypeScript",
 };
 
 export default function RootLayout({
-    children,
+  children,
 }: {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en">
-            <body
-                className="bg-color-primary text-white tracking-wider"
-                suppressHydrationWarning={false}
-            >
-                <NavBar />
-                {children}
-                <FooterBar />
-                <Script
-                    src="https://kit.fontawesome.com/8330d3dfaf.js"
-                    crossOrigin="anonymous"
-                />
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body
+        className="bg-color-primary text-white tracking-wider"
+        suppressHydrationWarning={false}
+      >
+        <NavBar />
+        {children}
+        <FooterBar />
+        <Script
+          src="https://kit.fontawesome.com/8330d3dfaf.js"
+          crossOrigin="anonymous"
+        />
+      </body>
+    </html>
+  );
 }
