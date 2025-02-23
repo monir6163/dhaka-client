3ff3f075bf7b360c0071a7cef2243d1f40f41865
monir6163 <monirhossain6163@gmail.com>
2025-02-23 15:54:28 +0600

    done

--- src/app/components/Shared/NavBar.tsx
+++ src/app/components/Shared/NavBar.tsx
@@ -91,7 +91,7 @@ const NavBar = () => {
           <nav className="lg:container md:px-6 flex justify-between items-center">
             <div className="py-5 text-color-secondary font-bold text-3xl">
               <Link href="/" className="ease-in duration-200">
-                <span className="text-white">SASS</span>app
+                <span className="text-white">SASS</span>work
               </Link>
             </div>
             <div>
